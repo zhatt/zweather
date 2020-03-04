4567e4115fb468e74277c4e18c8dac54baa88e29
Craig Robson <craig@zhatt.com>
2020-03-03 20:46:54 -0800

    Cleanup spacing.

--- weather_edge_server.cpp
+++ weather_edge_server.cpp
@@ -14,32 +14,33 @@ int main()
     zmq::context_t context{1};
 
     // construct a socket and bind to interface for receiving data.
-    zmq::socket_t in_socket{context, ZMQ_PULL};
-    in_socket.bind("tcp://*:5555");
+    zmq::socket_t in_socket{ context, ZMQ_PULL };
+    in_socket.bind( "tcp://*:5555" );
 
     // construct a socket and bind to interface for sending to SQL store.
-    zmq::socket_t out_sql_store_socket{context, ZMQ_PUSH};
-    out_sql_store_socket.connect("tcp://localhost:5556");
+    zmq::socket_t out_sql_store_socket{ context, ZMQ_PUSH };
+    out_sql_store_socket.connect( "tcp://localhost:5556" );
 
     // construct a socket and bind to interface for sending to raw store.
-    zmq::socket_t out_raw_store_socket{context, ZMQ_PUSH};
-    out_raw_store_socket.connect("tcp://localhost:5557");
+    zmq::socket_t out_raw_store_socket{ context, ZMQ_PUSH };
+    out_raw_store_socket.connect( "tcp://localhost:5557" );
 
     for (;;) {
         zmq::message_t message_in;
 
         // receive a request from client
-        in_socket.recv(message_in, zmq::recv_flags::none);
+        in_socket.recv( message_in, zmq::recv_flags::none );
 
         struct timeval receive_time;
-        gettimeofday(&receive_time, NULL);
+        gettimeofday( &receive_time, NULL );
 
-        std::string data(reinterpret_cast<const char*>(message_in.data()));
+        std::string data( reinterpret_cast<const char*>( message_in.data() ) );
         zweather::WeatherData data_point;
-        data_point.ParseFromString(data);
+        data_point.ParseFromString( data );
 
-        data_point.mutable_receive_time()->set_seconds(receive_time.tv_sec);
-        data_point.mutable_receive_time()->set_nanos(receive_time.tv_usec * 1000);
+        //FIXME validate buffer.  It comes from untrusted client.
+        data_point.mutable_receive_time()->set_seconds( receive_time.tv_sec );
+        data_point.mutable_receive_time()->set_nanos( receive_time.tv_usec * 1000 );
 
         // FIXME remove or make trace conditional.
         std::cout << "Received " << std::endl;
@@ -47,20 +48,20 @@ int main()
 
         // Reserialize with receive time.
         std::string buf;
-        bool good = data_point.SerializeToString(&buf);
-        if (! good) {
+        bool good = data_point.SerializeToString( &buf );
+        if ( ! good ) {
             std::cerr << "Error serializing" << std::endl;
             return 1;
         }
-        zmq::message_t message_to_sql_store(buf.size());
-        memcpy(message_to_sql_store.data(), buf.c_str(), buf.size());
+        zmq::message_t message_to_sql_store( buf.size() );
+        memcpy( message_to_sql_store.data(), buf.c_str(), buf.size() );
 
         // Send the message to both the sql and raw store backends.
         zmq::message_t message_to_raw_store;
-        message_to_raw_store.copy(message_to_sql_store);
+        message_to_raw_store.copy( message_to_sql_store );
 
-        out_sql_store_socket.send(message_to_sql_store);
-        out_raw_store_socket.send(message_to_raw_store);
+        out_sql_store_socket.send( message_to_sql_store );
+        out_raw_store_socket.send( message_to_raw_store );
     }
 
     return 0;
